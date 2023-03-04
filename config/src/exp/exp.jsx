105dc6552a92472f4e681abbfebcfb2743cbcc4a
anantadhia <102227869+anantadhia@users.noreply.github.com>
2023-03-04 13:50:17 +0700

    deorient

--- config/src/exp/exp.jsx
+++ config/src/exp/exp.jsx
@@ -35,45 +35,45 @@ const Experience = () => {
 
   return (
     <>
-      <DeviceOrientationControls />
-      <PresentationControls
-        speed={0.6}
-        global
-        zoom={0.5}
-        polar={[-1, Math.PI / 4]}
-      >
-        <Stars
-          radius={100}
-          depth={50}
-          count={2000}
-          factor={4}
-          saturation={5}
-          fade
-          speed={2}
-        />
-        <Stage environment={"forest"} intensity={0.6} contactShadow={false}>
-          <mesh>
-            <Float
-              speed={1} // Animation speed, defaults to 1
-              rotationIntensity={1} // XYZ rotation intensity, defaults to 1
-              floatIntensity={1} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
-              floatingRange={[0.2, 0.4]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
-            >
-              <SparklesContainer
-                sparkles={[
-                  { color: "rgb(0, 255, 76)", count: 3 },
-                  { color: "rgb(245, 226, 17)", count: 7 },
-                  { color: "rgb(222, 2, 2)", count: 9 },
-                  { color: "rgb(214, 53, 9)", count: 1 },
-                ]}
-              />
-              <Suspense fallback={null}>
-                <primitive object={gltf.scene} scale={0.1} />
-              </Suspense>
-            </Float>
-          </mesh>
-        </Stage>
-        {/* <mesh position={[0, -20, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+      <DeviceOrientationControls>
+        <PresentationControls
+          speed={0.6}
+          global
+          zoom={0.5}
+          polar={[-1, Math.PI / 4]}
+        >
+          <Stars
+            radius={100}
+            depth={50}
+            count={2000}
+            factor={4}
+            saturation={5}
+            fade
+            speed={2}
+          />
+          <Stage environment={"forest"} intensity={0.6} contactShadow={false}>
+            <mesh>
+              <Float
+                speed={1} // Animation speed, defaults to 1
+                rotationIntensity={1} // XYZ rotation intensity, defaults to 1
+                floatIntensity={1} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+                floatingRange={[0.2, 0.4]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+              >
+                <SparklesContainer
+                  sparkles={[
+                    { color: "rgb(0, 255, 76)", count: 3 },
+                    { color: "rgb(245, 226, 17)", count: 7 },
+                    { color: "rgb(222, 2, 2)", count: 9 },
+                    { color: "rgb(214, 53, 9)", count: 1 },
+                  ]}
+                />
+                <Suspense fallback={null}>
+                  <primitive object={gltf.scene} scale={0.1} />
+                </Suspense>
+              </Float>
+            </mesh>
+          </Stage>
+          {/* <mesh position={[0, -20, 0]} rotation={[-Math.PI / 2, 0, 0]}>
           <planeGeometry args={[170, 170]} />
           <MeshReflectorMaterial
             blur={[1000, 1000]}
@@ -88,7 +88,8 @@ const Experience = () => {
             metalness={0.5}
           />
         </mesh> */}
-      </PresentationControls>
+        </PresentationControls>
+      </DeviceOrientationControls>
       <CameraShake
         maxYaw={0.1} // Max amount camera can yaw in either direction
         maxPitch={0.1} // Max amount camera can pitch in either direction
